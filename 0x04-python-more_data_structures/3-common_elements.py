4df8c95141199aec27805f85e2395803c8960a1d
abel <abelamare3022@gmail.com>
2023-10-12 05:31:40 +0300

    h

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -6,4 +6,5 @@ def common_elements(set_1, set_2):
     A function that returns a set of
     common elements in two sets
     """
-    return(set_1 & set_2)
+    common_set = set_1.intersection(set_2)
+    return common_set
