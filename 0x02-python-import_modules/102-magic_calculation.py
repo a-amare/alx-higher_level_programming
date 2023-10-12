c13e504445684a434f43ebd7bbf722191d3b4cc1
abel <abelamare3022@gmail.com>
2023-10-12 05:53:26 +0300

    afsd

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,13 +2,19 @@
 
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-    def add(x, y):
-        return x + y
-
-    def sub(x, y):
-        return x - y
 
     if a < b:
-        return add(a, b)
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
     else:
-        return sub(a, b)
+        return(sub(a, b))
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
