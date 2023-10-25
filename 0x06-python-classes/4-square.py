14eab8d7e67269481ffbe5cca9e76011f52d574d
abel <abelamare3022@gmail.com>
2023-10-25 05:40:32 +0300

    hg

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,51 +1,31 @@
 #!/usr/bin/python3
-
-"""Square Class with size validation and area calculation"""
+"""Define a class Square."""
 
 
 class Square:
-    """Square class"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """
-        Initializes a Square instance with a specified size.
+        """Initialize a new square.
 
         Args:
-            size (int): The size of the square (default is 0).
+            size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
+    @property
     def size(self):
-        """
-        Get the size of the square.
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        Returns:
-            int: The size of the square.
-        """
-        return self.__size
-
-    def size(self, size):
-        """
-        Set the size of the square.
-
-        Args:
-            size (int): The new size for the square.
-
-        Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
-        """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        """
-        Calculate and return the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
