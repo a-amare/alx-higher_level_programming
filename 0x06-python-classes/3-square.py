43f967eb4c85bfe4003eddaa72b9366f04a43a1e
abel <abelamare3022@gmail.com>
2023-10-25 04:42:05 +0300

    number 3?

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" Square Class with size validation """
+
+
+class Square:
+    """ Square class """
+
+    def __init__(self, size=0):
+        '''
+        Args:
+                size (int): size of square side, default set to zero
+        '''
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        ''' calculates area of a square '''
+        return self.__size ** 2
