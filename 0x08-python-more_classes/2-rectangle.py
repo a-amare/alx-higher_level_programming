fa492952c0c2e82182b168ce678c8d31e4d7225a
abel <abelamare3022@gmail.com>
2023-11-02 03:43:55 +0300

    pylnit?

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     @property
     def width(self):
         ''' width getter function '''
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -19,12 +19,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        return (self.__width)
+        return self.__width
 
     @property
     def height(self):
         '''getter function for height'''
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -34,7 +34,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        return (self.__height)
+        return self.__height
 
     def __init__(self, width=0, height=0):
         '''initializes with 0 if not supplied '''
@@ -51,12 +51,12 @@ class Rectangle:
 
     def area(self):
         ''' calcuates area of rectangle width*height '''
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
         ''' calcuates area of perimeter 2(width*height) '''
         if (self.__width == 0 or self.__height == 0):
-            return (0)
-        return (2 * (self.__height + self.__width))
+            return 0
+        return 2 * (self.__height + self.__width)
 
     # just a randm comment
